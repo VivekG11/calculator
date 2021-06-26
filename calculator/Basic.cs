f953e15444a8b3e71b62e4012fbe3b4abc575bf9
VivekG11 <vivekvk2903@gmail.com>
2021-06-26 22:01:50 +0530

    commiting all files

---
+++ calculator/Basic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calculator
+{
+    class Basic
+    {
+       public static void Def()
+        {
+            Console.WriteLine("Basic Application");
+        }
+    }
+}
---
+++ calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Add();
+            Console.WriteLine("Sathyabama University");
+            Basic.Def();
+
+        }
+        public static void Add()
+        {
+            Console.WriteLine("Performing Add");
+            int a = 11;
+            int b = 29;
+            Console.WriteLine("The sum is :"+(a+b));
+            Details();
+        }
+        public static void Details()
+        {
+            Console.WriteLine("Vivek");
+            Console.WriteLine("Tvs Next Fellowship Program");
+        }
+    }
+}
